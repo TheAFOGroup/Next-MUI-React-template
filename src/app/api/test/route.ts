26cffe166bfcabb6dd30101b0dfb594dfec0ce14
AprilChoiAFO <HauYi@theafogroup.com>
2024-08-08 15:29:56 +0100

    Update runtime

--- src/app/api/test/route.ts
+++ src/app/api/test/route.ts
@@ -1,5 +1,6 @@
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
 // Test local NextJs API /api/test method GET with parameters
 export const GET = async (req: Request) => {
   const { searchParams } = new URL(req.url);
