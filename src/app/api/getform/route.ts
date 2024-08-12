7ccb5916759930f37c32d17a14115520f86f616b
AprilChoiAFO <HauYi@theafogroup.com>
2024-08-12 14:53:45 +0100

    changed compactivility date and change env

--- src/app/api/getform/route.ts
+++ src/app/api/getform/route.ts
@@ -1,13 +1,10 @@
 export const runtime = 'edge';
-export interface Env {
-  DB: D1Database;
-}
 //import { getRequestContext } from "@cloudflare/next-on-pages";
 import { NextResponse } from 'next/server';
 
 export const GET = async () => {
   //const db = getRequestContext().env.DB
-  const stmt = context.env.DB.prepare('SELECT * FROM form_fields').all()
+  const stmt = (process.env.DB as any)?.prepare('SELECT * FROM form_fields').all()
 
   return NextResponse.json(JSON.stringify(stmt));
 };
