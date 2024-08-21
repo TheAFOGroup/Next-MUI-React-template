4d7fded60b3596bcc027935e5d076f30380d7b9a
AprilChoiAFO <HauYi@theafogroup.com>
2024-08-21 10:56:11 +0100

    add register example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { SignIn } from '@/components/auth/signinButton';
 import { SignOut } from '@/components/auth/signoutButton';
 import { SessionProvider } from "next-auth/react";
 import { auth } from '../../auth';
+import Register from '@/components/Register';
 
 const AppHome = async () => {
   const session = await auth();
@@ -14,7 +15,7 @@ const AppHome = async () => {
       <Box>
         session={JSON.stringify(session)}
         {session ? <SignOut /> : <SignIn />}
-        { }
+        <Register></Register>
       </Box>
     </SessionProvider>
 
